2495feb418d5d6f321e65010aa49715f57b63549
Ranjith-GIT <ranjithkumarrr.333@gmail.com>
2021-05-16 20:15:38 +0530

    SwappingNumbers

--- src/test/java/com/learning/java/SecondProject.java
+++ src/test/java/com/learning/java/SecondProject.java
@@ -1,10 +1,40 @@
 package com.learning.java;
 
 public class SecondProject {
-
+/*
+ * Fundamentals:
+ 1. Intro to JAVA
+ 2. Variables
+ 3. Data Types
+ 4. Operators
+ 5. Methods, Classes
+ 6. If, Else, Switch Cases - Conditions
+ 7. Loops (For, While, Do While, For Each)
+ 
+ Java Related:
+ 1. Static-Non Static Related
+ 2. Constructors - Constructor Chaining, Constructor Overloading
+ 3. Method Overloading, Method overriding
+ 4. Encapsulation - Get and Set Methods
+ 5. Inheritance
+ 6. Abstraction
+ 7. Casting (Upcasting, down casting)
+ 8. Object Class
+ 9. Strings
+ 10. String Classes
+ 11. Exception handling
+ 12. Collection (List & Map)
+ 
+ TestNG - Framework
+ Maven
+ Excel data reader and writing
+ Log files - Extent report
+ GIT
+ Jenkins
+ 
+ Selenium
+ */
 	public static void main(String[] args) {
-		System.out.println("This is My 2nd Program");
-
+		System.out.println("This is My 2nd Program");	 
 	}
-
 }
---
+++ src/test/java/com/learning/java/ThirdProject.java
@@ -0,0 +1,19 @@
+package com.learning.java;
+
+public class ThirdProject {
+
+	public static void main(String[] args) {
+
+int a=10, b=20;
+System.out.println("Before Swapping"+ a + " " + b);
+int t=a; // t = 10
+a=b; // a=20
+b=t; // b= 10
+
+
+System.out.println("After Swapping" + a + " "+ b);
+
+
+	}
+
+}
